c4d1fb69df021e574d38bf60b67952df982acb4b
nadran20 <audran.liard2@gmail.com>
2024-01-18 21:56:58 +0100

    add bubble

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,14 @@
 import Card from "@/app/ui/card/card";
 import './page.css';
 import Image from "next/image";
+import Bubble from "@/app/ui/bubble/bubble";
 
 export default function Dashboard() {
     return (
         <main>
             <div className={'list-card'}>
                 <Card title={'Nombre de vos ruches sous notre surveillance :'}>
-                    <div className={'flex items-center mt-14'}>
+                    <div className={'flex items-center mt-10'}>
                         <p className={'font-bold text-2xl mr-2'}>5</p>
                         <Image
                             src={'/beehive.svg'}
@@ -18,7 +19,7 @@ export default function Dashboard() {
                     </div>
                 </Card>
                 <Card title={'Température moyenne dans vos ruches :'}>
-                    <></>
+                    <Bubble value={20}/>
                 </Card>
                 <Card title={'Statistique des derniers mois pour l’ensemble de vos ruches'}>
                     <></>
---
+++ src/app/ui/bubble/bubble.tsx
@@ -0,0 +1,22 @@
+import './bubble.css'
+
+export interface BubbleProps {
+    value: number
+}
+
+export default function Bubble({value}: BubbleProps) {
+    return (
+        <div className={'bubble'} style={{backgroundColor: getColor(value)}}>
+            <div className={'bubble-value'}>
+                {value}
+            </div>
+        </div>
+    )
+}
+
+function getColor(value: number) {
+    const percent = (value - 12) / (40 - 12);
+    const red = Math.round(255 * percent);
+    const green = Math.round(255 * (1 - percent));
+    return `rgb(${red}, ${green}, 0)`
+}
